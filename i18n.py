e4ced7e071a31bd53afcea984c078d9829f2ad7b
Marc Masdeu <marc.masdeu@gmail.com>
2020-01-31 15:23:22 +0100

    Commented out unused internalization.

--- i18n.py
+++ i18n.py
@@ -1,11 +1,11 @@
-# English - UoG
-ident_str = 'Identifier'
-fullname_str = 'Full name'
-email_str = 'Email address'
-status_str = 'Status'
-grade_str = 'Grade'
-max_grade_str = 'Maximum Grade'
-submitted_str = 'Submitted'
+# # English - UoG
+# ident_str = 'Identifier'
+# fullname_str = 'Full name'
+# email_str = 'Email address'
+# status_str = 'Status'
+# grade_str = 'Grade'
+# max_grade_str = 'Maximum Grade'
+# submitted_str = 'Submitted'
 
 # Catalan  - UAB
 ident_str = 'Identificador'
